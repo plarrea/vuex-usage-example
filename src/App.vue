a9498ff9d393e956148302ba2e25ca80bfa28667
Pablo Larrea <larrea.pablo@gmail.com>
2026-02-25 14:21:31 -0500

    auth state example

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,22 @@
 <template>
-  <base-container title="Vuex">
+  <base-container title="Vuex" v-if="userIsAuthenticated">
     <TheCounter />
     <FavoriteValue />
     <ChangeCounter />
     <button @click="increase">Add 10</button>
   </base-container>
+  <base-container title="Auth">
+    <UserAuth />
+  </base-container>
 </template>
 
 <script>
+import { mapGetters } from "vuex";
 import BaseContainer from "./components/BaseContainer.vue";
 import ChangeCounter from "./components/ChangeCounter.vue";
 import FavoriteValue from "./components/FavoriteValue.vue";
 import TheCounter from "./components/TheCounter.vue";
+import UserAuth from "./components/UserAuth.vue";
 
 export default {
   components: {
@@ -19,11 +24,15 @@ export default {
     TheCounter,
     ChangeCounter,
     FavoriteValue,
+    UserAuth,
   },
   methods: {
     increase() {
       this.$store.dispatch("increase", { value: 10 });
     },
+  },
+  computed: {
+    ...mapGetters(["userIsAuthenticated"]),
   },
 };
 </script>
---
+++ src/components/UserAuth.vue
@@ -0,0 +1,23 @@
+<template>
+  <button v-if="!userIsAuthenticated" @click="login">Login</button>
+  <button v-else @click="logout">Logout</button>
+</template>
+
+<script>
+import { mapActions, mapGetters } from "vuex";
+
+export default {
+  methods: {
+    ...mapActions(["login", "logout"]),
+    // login() {
+    //   this.$store.dispatch("login");
+    // },
+    // logout() {
+    //   this.$store.dispatch("logout");
+    // },
+  },
+  computed: {
+    ...mapGetters(["userIsAuthenticated"]),
+  },
+};
+</script>
