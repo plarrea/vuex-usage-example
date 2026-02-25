3137b12435938e2dac5672765c91be5b14858c20
Pablo Larrea <larrea.pablo@gmail.com>
2026-02-25 12:49:53 -0500

    store and mutations example

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,26 @@
 <template>
-  <base-container title="Vuex"></base-container>
+  <base-container title="Vuex">
+    <TheCounter />
+    <ChangeCounter />
+    <button @click="increase">Add 10</button>
+  </base-container>
 </template>
 
 <script>
-import BaseContainer from './components/BaseContainer.vue';
+import BaseContainer from "./components/BaseContainer.vue";
+import ChangeCounter from "./components/ChangeCounter.vue";
+import TheCounter from "./components/TheCounter.vue";
 
 export default {
   components: {
     BaseContainer,
+    TheCounter,
+    ChangeCounter,
+  },
+  methods: {
+    increase() {
+      this.$store.commit("increase", { value: 10 });
+    },
   },
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,26 @@
-import { createApp } from 'vue';
+import { createApp } from "vue";
+import { createStore } from "vuex";
 
-import App from './App.vue';
+import App from "./App.vue";
+
+const store = createStore({
+  state() {
+    return {
+      counter: 0,
+    };
+  },
+  mutations: {
+    increment(state) {
+      state.counter = state.counter + 1;
+    },
+    increase(state, payload) {
+      state.counter = state.counter + payload.value;
+    },
+  },
+});
 
 const app = createApp(App);
 
-app.mount('#app');
+app.use(store);
+
+app.mount("#app");
