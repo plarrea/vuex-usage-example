f2f19fd187ebb79a5dec94962f4d9b08e564cb46
Pablo Larrea <larrea.pablo@gmail.com>
2026-02-25 13:44:19 -0500

    implement mapper helpers

--- src/components/ChangeCounter.vue
+++ src/components/ChangeCounter.vue
@@ -1,13 +1,21 @@
 <template>
-  <button @click="addOne">Add 1</button>
+  <button @click="increment">Add 1</button>
+  <button @click="increase({ value: 20 })">Add 20</button>
 </template>
 
 <script>
+import { mapActions } from "vuex";
+
 export default {
   methods: {
-    addOne() {
-      this.$store.dispatch("increment");
-    },
+    ...mapActions(["increment", "increase"]),
+    // ...mapActions({
+    //   inc1: "increment",
+    //   incX: "increase",
+    // }),
+    // addOne() {
+    //   this.$store.dispatch("increment");
+    // },
   },
 };
 </script>
